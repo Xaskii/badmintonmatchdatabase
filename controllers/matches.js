36faa96b1d05b31c1a6081e01cd8296986b30f29
Xaskii <21989858+Xaskii@users.noreply.github.com>
2022-12-07 11:36:20 -0500

    basic crud operations for matches

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,7 +1,26 @@
-const matchesRouter = require('express').Router()
+const matchRouter = require('express').Router()
+const Match = require('../models/match')
 
-matchesRouter.get('/', async (req, res) => {
-  res.json({ msg: 'hi' })
+matchRouter.get('/', async (req, res) => {
+  const matches = await Match.find({}).populate('user', 'username')
+  res.json(matches)
 })
 
-module.exports = matchesRouter
+matchRouter.post('/', async (req, res) => {
+  const body = req.body
+  const match = new Match(body)
+
+  const savedMatch = await match.save()
+  res.status(201).json(savedMatch)
+})
+
+matchRouter.put('/:id', async (req, res) => {
+  await Match.findByIdAndUpdate(req.params.id)
+  res.status(200).end()
+})
+
+matchRouter.delete('/', async (req, res) => {
+  await Match.deleteMany({})
+  res.status(204).end()
+})
+module.exports = matchRouter
