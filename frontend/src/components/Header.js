201b2f5a2410f9fe2121f5d526fb61a46a021510
Xaskii <21989858+Xaskii@users.noreply.github.com>
2023-01-02 00:51:24 -0500

    refactored header into file

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/react'
+
+const Header = () => {
+  return (
+    <Heading mx={2} my={1} textAlign='center'>
+      Badminton Match Database
+    </Heading>
+  )
+}
+
+export default Header
