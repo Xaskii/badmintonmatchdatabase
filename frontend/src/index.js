0c44ea7e8739e32ab2217aa750f9526070139a2c
Xaskii <21989858+Xaskii@users.noreply.github.com>
2023-01-02 00:51:57 -0500

    added react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
@@ -10,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
     <ChakraProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
     </ChakraProvider>
   </Provider>
 )
